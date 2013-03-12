90987ea2c91f1c7c0d106881edf97dc416a62b2c
hiro-su <h.sugipon@gmail.com>
2013-03-12 10:25:07 +0900

    modify problem5

--- problems/problem5.rb
+++ problems/problem5.rb
@@ -1,46 +1,15 @@
+def smallest n, value
+  throw :exit if n == 0
+  if value % n == 0
+    puts value if n-1 == 1
+    smallest n-1, value
+  end
+end
+
 catch :exit do
-  999999999.times do |n|
-    if n % 2 == 0
-      if n % 3 == 0
-        if n % 4 == 0
-          if n % 5 == 0
-            if n % 6 == 0
-              if n % 7 == 0
-                if n % 8 == 0
-                  if n % 9 == 0
-                    if n % 10 == 0
-                      if n % 11 == 0
-                        if n % 12 == 0
-                          if n % 13 == 0
-                            if n % 14 == 0
-                              if n % 15 == 0
-                                if n % 16 == 0
-                                  if n % 17 == 0
-                                    if n % 18 == 0
-                                      if n % 19 == 0
-                                        if n % 20 == 0
-                                          unless n == 0
-                                            puts n
-                                            throw :exit
-                                          end
-                                        end
-                                      end
-                                    end
-                                  end
-                                end
-                              end
-                            end
-                          end
-                        end
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-    end
+  n,value = 20,1
+  loop do
+    smallest n, value
+    value += 1
   end
 end
