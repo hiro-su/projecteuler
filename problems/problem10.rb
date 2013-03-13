f16cf6f086fb785fd78792455f3206dea6225530
hiro-su <h.sugipon@gmail.com>
2013-03-13 12:42:13 +0900

    modify

--- problems/problem10.rb
+++
@@ -1,5 +0,0 @@
-require "prime"
-limit = 2000000
-result = 0
-Prime.each(limit) {|n| result += n }
-p result
--- problems/problem3.rb
+++
@@ -1,4 +0,0 @@
-require "prime"
-
-n = 600851475143
-puts Prime.prime_division(n).last[0]
